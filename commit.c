6fa83df5f67a1c4497c356ee1d56ea3e939b171c
Rhea Sushanth <rheasush@gmail.com>
2026-04-20 23:23:21 +0530

    Phase 4: serialize commit and store object

--- commit.c
+++ commit.c
@@ -209,4 +209,16 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     snprintf(c.author, sizeof(c.author), "%s", pes_author());
     c.timestamp = (uint64_t)time(NULL);
     snprintf(c.message, sizeof(c.message), "%s", message);
+    void *data;
+    size_t len;
+
+    if (commit_serialize(&c, &data, &len) != 0)
+      return -1;
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+      free(data);
+      return -1;
+}
+
+free(data);
 }
