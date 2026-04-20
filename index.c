18be1d195f42475425ccc8effd2b85f6b4c5f9d8
Rhea Sushanth <rheasush@gmail.com>
2026-04-20 23:07:37 +0530

    Phase 3: implement index persistence to disk

--- index.c
+++ index.c
@@ -173,10 +173,23 @@ int index_status(const Index *index) {
 //
 // Returns 0 on success, -1 on error.
 int index_save(const Index *index) {
-    // TODO: Implement atomic index saving
-    // (See Lab Appendix for logical steps)
-    (void)index;
-    return -1;
+    FILE *fp = fopen(".pes/index", "w");
+    if (!fp) return -1;
+
+    for (int i = 0; i < index->count; i++) {
+        char hex[65];
+        hash_to_hex(&index->entries[i].hash, hex);
+
+        fprintf(fp, "%o %s %u %u %s\n",
+                index->entries[i].mode,
+                hex,
+                index->entries[i].mtime_sec,
+                index->entries[i].size,
+                index->entries[i].path);
+    }
+
+    fclose(fp);
+    return 0;
 }
 
 // Stage a file for the next commit.
